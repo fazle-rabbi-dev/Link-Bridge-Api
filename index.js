8b5a922e52c580e59fd8775b8af540aff8a42d35
fh-rabbi <frabby983@gmail.com>
2024-03-03 11:56:09 +0600

    Trying to deploy on cyclic

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from "express"
+
+const app = express()
+
+app.all('/', (req, res) => {
+    console.log("Just got a request!")
+    res.send('Yo!')
+})
+
+app.listen(process.env.PORT || 3000)
